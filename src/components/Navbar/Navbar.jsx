db9bada03a9f3f19718cf3b8d586757244cc5db4
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-01-10 14:44:15 -0500

    navbar almos fully done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // const icon = <Icon icon="material-symbols:menu-rounded" />;
+
   return (
     <header>
       <nav className="c-navbar">
@@ -10,26 +15,76 @@ const Navbar = () => {
           placeholder="Seach for movies..."
           name="search"
         />
-        <ul className="c-navbar__list">
-          <li className="c-navbar__link">
-            <Link to={"/popular"}>
-              <p className="c-navbar__a">Popular</p>
+        <ul className={`c-navbar__nav-menu ${isOpen ? "show" : ""}`}>
+          <li>
+            <Link to="/" className="c-navbar__a">
+              Home
             </Link>
           </li>
-          <li className="c-navbar__link">
-            <Link to={"/top-rated"}>
-              <p className="c-navbar__a">Top Rated</p>
+          <li>
+            <Link to="/popular" className="c-navbar__a">
+              Popular
             </Link>
           </li>
-          <li className="c-navbar__link">
-            <Link to={"/upcoming"}>
-              <p className="c-navbar__a">Upcoming</p>
+          <li>
+            <Link to="/top-rated" className="c-navbar__a">
+              Top Rated
+            </Link>
+          </li>
+          <li>
+            <Link to="/upcoming" className="c-navbar__a">
+              Upcoming
             </Link>
           </li>
         </ul>
+        <div className="c-navbar__menu-icon" onClick={() => setIsOpen(!isOpen)}>
+          <i className={`c-navbar__fas fa-bars ${isOpen ? "open" : ""}`}>
+            {
+              <Icon
+                icon="material-symbols:menu-rounded"
+                color="white"
+                width="35"
+                height="35"
+              />
+            }
+          </i>
+        </div>
       </nav>
     </header>
   );
 };
-
+{
+  /* <header>
+      <nav className="c-navbar">
+        <input
+          className="c-navbar__search"
+          type="search"
+          placeholder="Seach for movies..."
+          name="search"
+        />
+        <ul className="c-navbar__list">
+          <li className="c-navbar__link">
+            <Link to={"/"} className="c-navbar__a">
+              Home
+            </Link>
+          </li>
+          <li className="c-navbar__link">
+            <Link to={"/popular"} className="c-navbar__a">
+              Popular
+            </Link>
+          </li>
+          <li className="c-navbar__link">
+            <Link to={"/top-rated"} className="c-navbar__a">
+              Top Rated
+            </Link>
+          </li>
+          <li className="c-navbar__link">
+            <Link to={"/upcoming"} className="c-navbar__a">
+              Upcoming
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header> */
+}
 export default Navbar;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div className="c-slider">
+      <button>Atras</button>
+      <div className="c-slider__images-container"></div>
+      <button>Adelante</button>
+    </div>
+  );
 };
 
 export default Slider;
