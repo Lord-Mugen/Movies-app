321b30cec10fa638316996e0f5fbe060ee67c923
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-01-10 10:12:19 -0500

    navbar done already

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,32 @@
-import { Icon } from "@iconify/react";
-
 const Navbar = () => {
   return (
-    <div>
+    <header>
       <nav className="c-navbar">
-        <input type="search" placeholder="Seach for movies" name="search" />
-        <input type="checkbox" id="check" />
-        <label for="check" className="c-navbar__checkbtn">
-          <Icon icon="material-symbols:menu-rounded" />
-        </label>
+        <input
+          className="c-navbar__search"
+          type="search"
+          placeholder="Seach for movies..."
+          name="search"
+        />
         <ul className="c-navbar__list">
           <li className="c-navbar__link">
-            <a href="#">Popular</a>
+            <a className="c-navbar__a" href="#">
+              Popular
+            </a>
           </li>
           <li className="c-navbar__link">
-            <a href="#">Top Rated</a>
+            <a className="c-navbar__a" href="#">
+              Top Rated
+            </a>
           </li>
           <li className="c-navbar__link">
-            <a href="#">Upcoming</a>
+            <a className="c-navbar__a" href="#">
+              Upcoming
+            </a>
           </li>
         </ul>
       </nav>
-
-      {/* <nav className="c-navbar">
-        <input
-          className="c-navbar__input"
-          type="search"
-          placeholder="Search for movies"
-        />
-
-        <ul className="c-navbar__list">
-          <li className="c-navbar__link">
-            <a href="">Popular</a>
-          </li>
-          <li className="c-navbar__link">
-            <a href="">Top Rated</a>
-          </li>
-          <li className="c-navbar__link">
-            <a href="">Upcoming</a>
-          </li>
-        </ul>
-      </nav> */}
-    </div>
+    </header>
   );
 };
 
