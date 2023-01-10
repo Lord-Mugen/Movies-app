6c2f9fd070a928bf68377757876461e04500e906
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-01-10 11:09:40 -0500

    to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/pages/Home/Home";
+import Popular from "./components/pages/Popular/Popular";
+import TopRated from "./components/pages/Top Rated/TopRated";
+import Upcoming from "./components/pages/Upcoming/Upcoming";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <h1>hola mundo</h1>
     </div>
   );
 }
