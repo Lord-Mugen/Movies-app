9985965905728b9ad2d2b6591e4c4e9084235696
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-01-11 16:18:24 -0500

    filter init

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,14 @@
+const Filter = () => {
+  return (
+    <div className="c-filter">
+      <p>Filter By</p>
+      <hr />
+      <h3>Genre</h3> {/* dropdown con las categorias */}
+      <form action="">
+        <input type="search" placeholder="Start typing to search..." />
+      </form>
+    </div>
+  );
+};
+
+export default Filter;
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import Slider from "../../components/Slider/Slider";
+import Filter from "../../components/Filter/Filter";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Slider />
+      <Filter />
     </div>
   );
 };
