1b1022b5899e9e08b48c43141795782dbcccda08
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-01-11 19:43:39 -0500

    slider problems

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 
 const Slider = () => {
@@ -12,10 +12,11 @@ const Slider = () => {
       height="20"
     />
   );
-
+  //state para la posición del slider
+  const [position, setPosition] = useState(0);
+  //state para la posición de cada div
   const [currentSlide, setCurrentSlide] = useState(0);
-
-  /* Dejar un maximo de 5 imagenes en el slider */
+  // array de imagenes
   const images = [
     "https://picsum.photos/800/350",
     "https://picsum.photos/800/350",
@@ -24,31 +25,53 @@ const Slider = () => {
     "https://picsum.photos/800/350",
   ];
 
+  //crea un estilo para cada imagen
+  const imageStyles = images.map((image) => ({
+    backgroundImage: `url(${image})`,
+  }));
+
+  //función para la animación
+  const animate = (direction) => {
+    //Se guarda la posición previa
+    const prevPos = currentSlide;
+    //se asigna la posición siguiente
+    setCurrentSlide((currentSlide + direction + images.length) % images.length);
+    //si es izquierda se aumenta la posición actual
+    if (direction === -1) {
+      setPosition(position - 1);
+    }
+    //si es derecha se disminuye
+    else {
+      setPosition(position + 1);
+    }
+    //se asigna un timeout para la animación suave
+    setTimeout(() => {
+      //se resetea la posición previa
+      setPosition((prevPos + direction + images.length) % images.length);
+    }, 500);
+  };
+
   return (
     <div className="c-slider">
-      <div className="c-slider__images-container">
-        {images.map((image, index) => (
-          <img
-            className="c-slider__image"
-            src={image}
-            key={index}
-            style={{
-              transform: `translateX(${index - currentSlide * 99.5}%)`,
-            }}
-          />
-        ))}
-      </div>
+      {images.map((image, index) => (
+        <div
+          key={image}
+          className="c-slider__images-container"
+          style={{
+            ...imageStyles[index],
+            transform: `translateX(${(index - position) * 100}%)`,
+          }}
+        ></div>
+      ))}
       <button
         className="c-slider__btn c-slider__btn-left"
-        onClick={() =>
-          setCurrentSlide((currentSlide - 1 + images.length) % images.length)
-        }
+        onClick={() => animate(-1)}
       >
         {leftIcon}
       </button>
       <button
         className="c-slider__btn c-slider__btn-right"
-        onClick={() => setCurrentSlide((currentSlide + 1) % images.length)}
+        onClick={() => animate(1)}
       >
         {rightIcon}
       </button>
